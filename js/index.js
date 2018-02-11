66411dca0e6cb1147b1e69aed25293ca6a6c0956
Brandon Richardson <brandon1024.br@gmail.com>
2018-02-11 18:42:29 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('a[href*="#"]:not([href="#"])').each(function() {
+        if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname && this.hash.replace(/#/,'')) {
+            var $targetId = $(this.hash);
+            var $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+
+            var $target;
+            if($targetId.length)
+                $target = $targetId;
+            else if($targetAnchor.length)
+                $target = $targetAnchor;
+            else
+                $target = false;
+
+            if($target) {
+                var targetOffset = $target.offset().top;
+                $(this).click(function() {
+                    $("#nav li a").removeClass("active");
+                    $(this).addClass('active');
+                    $('html, body').animate({scrollTop: targetOffset}, 1000);
+                    return false;
+                });
+            }
+        }
+    });
+});
